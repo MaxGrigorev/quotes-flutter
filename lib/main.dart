aacc539ee9ef8f9d84f24501b6580107e5d01ccd
Maksim Grigoryev <m.grigoryev@id-east.ru>
2021-03-23 10:33:50 +0500

    star button

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,131 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Quotes'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+Future<List<Quote>> getQuotes() async {
+  http.Response response = await http.get(
+      Uri.https("poloniex.com", "/public", {'command': 'returnTicker'}),
+      headers: {"Accept": "application/json"});
+  var result = json.decode(response.body) as Map<String, dynamic>;
+  var quotes = <Quote>[];
+
+  result.forEach((k, v) => quotes.add(Quote(k, v['last'], v['percentChange'])));
+  print('result');
+  return quotes;
+}
+
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: Container(
+        child: FutureBuilder(
+          future: getQuotes(),
+          builder: (BuildContext context, AsyncSnapshot<List<Quote>> snapshot) {
+            print('snapshot $snapshot');
+            if (snapshot.hasData) {
+              print('snapshot.hasData');
+              final List<Quote> data = snapshot.data;
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) =>
+                      QuoteWidget(quote: data[index]));
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Quote {
+  String pairName;
+  String last;
+  String percentChange;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Quote(this.pairName, this.last, this.percentChange);
+
+  @override
+  String toString() {
+    return '{Quote ${this.pairName}, ${this.last} ${this.percentChange}}';
   }
+}
+
+class QuoteWidget extends StatelessWidget {
+  QuoteWidget({Key key, @required this.quote}) : super(key: key);
+  final Quote quote;
+
+  Function onPress(String name) =>
+      (bool pressed) => print('name $name pressed $pressed');
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+        child: ListTile(
+          title: Text(quote.pairName,
+              style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600)),
+          subtitle: Text(
+            '${quote.last} ${quote.percentChange}%',
+            style: TextStyle(fontSize: 16.0),
+          ),
+          trailing: StarButton(onPress: onPress(quote.pairName)),
+        ));
+  }
+}
+
+class StarButton extends StatefulWidget {
+  StarButton({@required this.onPress});
+  final Function onPress;
+
+  @override
+  _StarButton createState() => _StarButton();
+}
+
+class _StarButton extends State<StarButton> {
+  bool pressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: const Icon(Icons.star_border),
+        tooltip: 'Add to favorit',
+        onPressed: () {
+          setState(() {
+            pressed = !pressed;
+          });
+          widget.onPress(pressed);
+        },
+        iconSize: 30,
+        color: pressed ? Colors.yellow : null);
   }
 }
